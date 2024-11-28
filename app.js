8baa804ac9a14825536891ffcf5fb2a7d29e7b0c
SanskarOmer <sanskaromer7@gmail.com>
2024-11-29 00:33:10 +0530

    create a folder structure for user routes and created a test route

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const userRouter = require('./routes/user.routes');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
+app.set('view engine', 'ejs');
+
+app.use('/user',userRouter);
 
 
 
