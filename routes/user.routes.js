f8c20b0c22a435a0b75f6a68a99ba50791e64604
Sanskar Omer <sanskaromer7@gmail.com>
2024-11-29 13:58:04 +0530

    added validation of email username and password using middleware named express-validator

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body,validationResult } = require('express-validator');
 
 
 
@@ -7,9 +8,21 @@ router.get('/register', (req, res) => {
     res.render('register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', 
+
+    body('email').trim().isEmail().isLength({ min:10}),
+    body('password').isLength({ min: 8 }),
+    body('username').trim().isLength({ min:5}),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(), 
+                message: 'Invalid Data',
+            });
+        }
     console.log(req.body);
-    res.send('Registration successful');
+    res.send(errors);
 });
 
 
