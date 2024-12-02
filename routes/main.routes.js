60bc7760c7094396bb89edd6f8322b4504c12b48
SanskarOmer <sanskaromer7@gmail.com>
2024-12-02 23:59:59 +0530

    added download functionality with file showing

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -5,9 +5,20 @@ const upload = require('../config/multer.config');
 const fileModel = require('../models/file.model');
 
 const authTokenMiddleware = require('../middleware/auth');
+const file = require('../models/file.model');
 
-router.get('/home', authTokenMiddleware ,(req, res) => {
-    res.render('home');
+router.get('/home', authTokenMiddleware, (req, res) => {
+
+    const userFiles = await fileModel.find({
+        user: req.user.userId
+    }) I
+
+
+    res.render('home',{
+        files: userFiles,
+    });
+
+
 });
 
 router.post('/upload', authTokenMiddleware, upload.single('file'), async (req, res) => {
@@ -15,10 +26,25 @@ router.post('/upload', authTokenMiddleware, upload.single('file'), async (req, r
         path: req.file.path,
         originalname: req.file.originalname,
         user: req.user.userId,
-       
+
     });
 
     res.json(newFile);
 });
 
+router.get('/download/:path', authTokenMiddleware, async (req, res) => {
+        const loggedUser = req.user.userId;
+        const path = req.params.path;
+
+        const file = await fileModel.findOne({
+            path:path,
+            user: loggedUser,
+        });
+
+        if (!file) {
+            return res.status(401).json({
+                message: 'Unauthorized',
+            });
+        }
+});
 module.exports = router;
